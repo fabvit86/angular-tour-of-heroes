9b890244c415ea33eaed19d6ad9fd3703d971f7a
Fabrizio Vitelli <fabvit86@yahoo.it>
2017-09-09 21:59:00 +0200

    moved the list of heroes to the heroes.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // ngModel
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms' // ngModel
 
-import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { AppComponent } from './app.component'
+import { HeroDetailComponent } from './hero-detail.component'
+import { HeroesComponent } from './heroes.component'
+
+import { HeroService } from './hero.service'
 
 @NgModule({
   // list of application components, pipes, and directives that belong to the module:
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
   // list of the external modules imported and used by the app:
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  // The providers array tells Angular to create a fresh instance of the HeroService when it creates a component:
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/heroes.component.ts
@@ -0,0 +1,94 @@
+import { Component } from '@angular/core'
+import { Hero } from './hero'
+import { HeroService } from './hero.service'
+import { OnInit } from '@angular/core'
+
+@Component({
+  // id of the html element that will contain the template:
+  selector: 'my-heroes',
+  // html of the component:
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <hero-detail [hero]="selectedHero"></hero-detail>
+    `,
+  styles: [`
+      .selected {
+        background-color: #CFD8DC !important;
+        color: white;
+      }
+      .heroes {
+        margin: 0 0 2em 0;
+        list-style-type: none;
+        padding: 0;
+        width: 15em;
+      }
+      .heroes li {
+        cursor: pointer;
+        position: relative;
+        left: 0;
+        background-color: #EEE;
+        margin: .5em;
+        padding: .3em 0;
+        height: 1.6em;
+        border-radius: 4px;
+      }
+      .heroes li.selected:hover {
+        background-color: #BBD8DC !important;
+        color: white;
+      }
+      .heroes li:hover {
+        color: #607D8B;
+        background-color: #DDD;
+        left: .1em;
+      }
+      .heroes .text {
+        position: relative;
+        top: -3px;
+      }
+      .heroes .badge {
+        display: inline-block;
+        font-size: small;
+        color: white;
+        padding: 0.8em 0.7em 0 0.7em;
+        background-color: #607D8B;
+        line-height: 1em;
+        position: relative;
+        left: -1px;
+        top: -4px;
+        height: 1.8em;
+        margin-right: .8em;
+        border-radius: 4px 0 0 4px;
+      }
+  `]
+})
+export class HeroesComponent implements OnInit{
+  // component properties:
+  heroes: Hero[]
+  selectedHero: Hero // specify that selectedHero is an instance of the Hero class
+
+  constructor(private heroService: HeroService) { }
+  
+  // get the array of heros from calling getHeroes() on the istance of HeroService service:
+  getHeroes(): void { 
+    // heroService.getHeroes() returns a Promise. When it resolves, the callback assign the heroes array to this.heroes:
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes)
+  }
+
+  // everything inside ngOnInit will be called when the component is initialized:
+  ngOnInit(): void {
+    this.getHeroes()
+  }
+
+  // hero click handler:
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
+  }
+}
